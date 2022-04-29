d36dd363bace35c67cef42d64f5ac54062cf5524
kaushal soni <kaushal95300@gmail.com>
2022-04-29 13:48:48 +0530

    GKE code added

---
+++ GKE/resource.tf
@@ -0,0 +1,45 @@
+# cluster
+resource "google_container_cluster" "primary" {
+  count = length(var.node_pool_region)
+  name     = "${var.cluster_name }-${ var.node_pool_region[count.index] }"
+  location = var.region
+  description = "Terraform test cluster"
+  network = var.vpc_name
+  resource_labels = var.labels
+  subnetwork = var.subnet
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  node_locations = var.node_zones
+
+}
+
+# node
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  depends_on = [
+    google_container_cluster.primary
+  ]
+  count = length(var.node_pool_region)
+  
+  name       = "${var.cluster_name}-node-pool-${count.index}"
+  location   = element(var.node_pool_region, count.index)
+  cluster    = element(google_container_cluster.primary.*.id , count.index)
+  node_count = 1
+
+  node_locations = var.node_zones
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+    labels = var.labels
+    tags = var.network_tags
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    #service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
---
+++ GKE/varaiable.tf
@@ -0,0 +1,55 @@
+variable "project_id" {
+  default = "searce-playground-v1"
+  description = "The Google Cloud project ID to use"
+}
+
+variable "region" {
+    default = "asia-south1"
+    description = "The Google Cloud region to use"
+}
+
+variable "node_pool_region" {
+    default = ["asia-south1", "asia-south2"]
+    description = "The Google Cloud region to use for the node pool"  
+}
+
+variable "node_zones" {
+    default = ["asia-south1-a"]
+    description = "The Google Cloud regions to use for nodes"
+  
+}
+
+# labelling
+variable "labels" {
+    default = {
+        "env" = "dev"
+        "owner" = "kush"
+    }
+    description = "Labels to apply to the instance"
+}
+
+# Networking
+variable "vpc_name" {
+    default = "kaushal-vpc"
+
+    description = "The name of the VPC to create"
+}
+
+variable "subnet" {
+    default = "kaushal-subnet-1"
+    description = "The name of the subnet to create"
+}
+
+variable "network_tags" {
+    default = ["kaushal-ssh-icmp","kaushal-flask"]
+    description = "The tags to apply to the network"
+}
+
+# Compute
+variable "cluster_name" {
+    default = "kaushal-gke-cluster"
+    description = "The name of the instance to create"
+}
+
+
+
